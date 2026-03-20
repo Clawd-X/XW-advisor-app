5c42df56aa128b77156a416cbb04dac276747813
Clawd-X <myclawdjourney@gmail.com>
2026-03-19 21:57:03 -0700

    deleted asset page, HOPEFULLY fixed google-genai

--- app.py
+++ app.py
@@ -10,10 +10,8 @@ st.write("Use the sidebar to navigate between tools.")
 
 p2 = st.Page("pages/p1.py", title = "Stats-Based College Advisor", icon = "📊")
 p3 = st.Page("pages/p2.py", title = "Short Answer Helper", icon = "✍️")
-p4 = st.Page("pages/p3.py", title = "WebdriverTorso", icon = "⚙️")
 
-
-pg = st.navigation([p2,p3,p4])
+pg = st.navigation([p2,p3])
 pg.run()
 
 st.markdown("""
